8e2c642579ad9f6e91b3652fc59645acdd1afa8b
sushma sush <sushmamr231@gmail.com>
2024-04-20 02:47:29 -0500

    Conditional Operator

---
+++ ConditionalOperatorDemo.java
@@ -0,0 +1,13 @@
+class ConditionalOperatorDemo{
+	
+	public static void main(String[] args){
+		
+		String res=(5<6)?"Hi":"Bye";
+		System.out.println(res);
+		int a=9;
+		String res1= (a%2==0)?"even":"bye";
+		System.out.println(res1);
+		
+	}
+	
+}
