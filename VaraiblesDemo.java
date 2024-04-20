fe74f96f992778a11456366297c6cb8b333b6529
sushma sush <sushmamr231@gmail.com>
2024-04-20 01:32:21 -0500

    Variables Demo

---
+++ VaraiblesDemo.java
@@ -0,0 +1,28 @@
+class VariablesDemo{
+	
+	public static void main(String[] args){
+		
+		int a=4;
+		int b=4;
+		System.out.println(a+b);
+		System.out.println(a+b);
+		
+		int c=1; //first time giving the value for c ---Initialization
+		System.out.println("Initialization: " +c);
+		
+		c=2; //Assignment
+		System.out.println("Assignment: " +c); 
+		
+		int d=10;
+		double e=20.1;
+		char f='q';
+		boolean g=true;
+
+		System.out.println("Integer: " +d);
+		System.out.println("Double: " +e);
+		System.out.println("Character: " +f);
+		System.out.println("Boolean: " +g);
+						
+	}
+	
+}
